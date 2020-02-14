5906341243be6fd2b167ff27712a9728f7b284a7
jonas <jonas@office.br>
2020-02-14 15:10:10 -0300

    initial commit

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-feminino/26/HZM-1280-026/HZM-1280-026_detalhe1.jpg?resize=280:280"
+                alt="imagem do produto"
+              />{' '}
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$ 1920,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 500,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
