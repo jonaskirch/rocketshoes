d040037d59924ab7a8e3caf37469a7cba1aa2617
Jonas Kirch <jonaskirch9@gmail.com>
2020-02-16 10:30:53 -0300

    add redux saga

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import store from './store';
+import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
-      <BrowserRouter>
+      <ToastContainer autoClose={3000} />
+      <Router history={history}>
         <Header />
         <Routes />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
